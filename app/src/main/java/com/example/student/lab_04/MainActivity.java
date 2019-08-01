c199b0e797289e0fe558959222424aba4c8b7cb0
NaminSaranga <naminsaranga54@gmail.com>
2019-08-01 13:03:42 +0530

    Toast Message

---
+++ app/src/main/java/com/example/student/lab_04/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.student.lab_04;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Context context = getApplicationContext();
+
+        int duration =Toast.LENGTH_LONG;
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.custom_layout_toast));
+
+        TextView text = (TextView) layout.findViewById(R.id.textImage);
+        text.setText("This is a custom toast");
+
+        Toast toast = new Toast(context);
+        toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
+        toast.setDuration(duration);
+        toast.setView(layout);
+        toast.show();
+
+
+    }
+}
